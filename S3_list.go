76d8d8dc1e447c7c4962a35b6fbffb6c78d1e102
Divyam <divyam.azad@tweeny.in>
2022-04-30 00:17:56 +0530

    List S3 bucket files

---
+++ S3_list.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+func main() {
+
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-west-2")},
+	)
+
+	// Create S3 service client
+
+	svc := s3.New(sess)
+
+	result, err := svc.ListBuckets(nil)
+	if err != nil {
+		exitErrorf("Unable to list buckets, %v", err)
+	}
+
+	fmt.Println("Buckets:")
+
+	for _, b := range result.Buckets {
+		fmt.Printf("* %s created on %s\n", aws.StringValue(b.Name), aws.TimeValue(b.CreationDate))
+	}
+}
+
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module S3_list.go
+
+go 1.18
+
+require github.com/aws/aws-sdk-go v1.44.4
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
